9e3364fb7f65a0dead14822c884f89124771a6e8
cozycole <tritchcole@gmail.com>
2024-03-21 20:11:53 -0700

    add js for including h2 element in video search result

--- public/js/search.js
+++ public/js/search.js
@@ -1,4 +1,5 @@
-document.getElementById('vidSearch')
+// Make it so enter button clicks button
+document.getElementById('searchInput')
     .addEventListener('keyup', e => {
         e.preventDefault()
         if (e.key === 'Enter') {
@@ -6,8 +7,9 @@ document.getElementById('vidSearch')
         }
     })
 
+// API call to search videos on button click
 document.getElementById('searchButton').addEventListener('click', function(event) {
-    const query = document.getElementById('vidSearch').value.trim();
+    const query = document.getElementById('searchInput').value.trim();
 
     fetch('/search/videos?search=' + encodeURIComponent(query))
     .then(response => response.json())
@@ -26,17 +28,20 @@ function displaySearchResults(results) {
     // Display new search results
     results.forEach(result => {
         const resultElement = document.createElement('div');
-        resultElement.textContent = result.title;
+
+        const resultTitle = document.createElement('h2');
+        resultTitle.textContent = result.title;
 
         const resultImg = document.createElement('img');
         resultImg.src = result.thumbnail_name;
 
         const resultLink = document.createElement('a');
         resultLink.href = result.video_url;
-        resultLink.target = "_blank";
+        resultLink.target = '_blank';
         
         resultLink.appendChild(resultImg)
         resultElement.append(resultLink)
+        resultElement.append(resultTitle)
         
         
         document.getElementById('searchResults').appendChild(resultElement);
